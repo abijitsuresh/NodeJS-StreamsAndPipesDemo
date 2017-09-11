f40b1f7dfbcfad4e0aebdb9b28eb17a6bb137104
Abijit Suresh Geetha Ram <abijit.suresh@outlook.com>
2017-09-10 21:49:16 -0500

    Stream and Pipe done.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var http = require('http');
 var fs = require('fs');
 
-var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var writeStream = fs.createWriteStream(__dirname + '/writeme.txt')
+// var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+// var writeStream = fs.createWriteStream(__dirname + '/writeme.txt');
 
-readStream.on('data', function(chunk){
-    console.log('New chunk received: ');
-    writeStream.write(chunk);
-});
-
-// var server = http.createServer(function(req, res){
-//     console.log('Request was made from: ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('This is from server!');
+// readStream.on('data', function(chunk){
+//     console.log('New chunk received: ');
+//     writeStream.write(chunk);
 // });
 
-// server.listen(3000, '127.0.0.1');
-// console.log("Listening to port 3000!");
+// Pipe
+// readStream.pipe(writeStream);
+
+var server = http.createServer(function(req, res){
+    console.log('Request was made from: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    readStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Listening to port 3000!");
